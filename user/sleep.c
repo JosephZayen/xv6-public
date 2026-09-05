d00948db8b8ffd2d364daf5540fee617a0b3e7cf
Joseph Zayen <xyj2068@gmail.com>
2026-09-05 15:36:44 +0800

    finished lab1 sleep.c

--- user/sleep.c
+++ user/sleep.c
@@ -5,7 +5,8 @@ int main(int argc, char *argv[]){
     if(argc < 1){
         exit(1);
     }
-    int ticks = atoi(argv);
+    int ticks = atoi(argv[1]);
+    printf("sleep %d", ticks);
     sleep(ticks);
     exit(0);
 }
